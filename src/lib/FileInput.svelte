590b3cf971ec026f45fca3b025e9fe2a0e68e43a
Neon <jfmsteam2@gmail.com>
2023-10-18 23:37:01 +0200

    test

---
+++ src/lib/FileInput.svelte
@@ -0,0 +1,27 @@
+<!-- FileInput.svelte -->
+<script lang="ts">
+
+    import type { ChangeEventHandler } from 'svelte/elements';
+
+	let selectedFile: File | null = null;
+
+	function handleFileInput(event: null | Event & { target: EventTarget & HTMLInputElement }) {
+		if (event) {
+			if (event.target) {
+				if (event.target.files) {
+					var file: File = event.target.files[0];
+					if (file) {
+						selectedFile = file;
+					}
+				}
+			}
+		}
+	}
+</script>
+
+<main>
+    <input type="file" on:change={handleFileInput} />
+	{#if selectedFile}
+		<p>Selected File: {selectedFile.name}</p>
+	{/if}
+</main>
